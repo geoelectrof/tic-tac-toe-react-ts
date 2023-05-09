44214f15abd5d523c3c4b035e6435bf641892e2f
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-05-09 14:51:39 +0300

    Basic styling and Square component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Square from './components/Square'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    console.log("Clicked!")
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>App</div>  
+      <div>       
+        <Square handleClick={handleClick}> </Square>
+        <Square> </Square>
+        <Square> </Square>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>       
+        <Square> </Square>
+        <Square> </Square>
+        <Square> </Square>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>       
+        <Square> </Square>
+        <Square> </Square>
+        <Square> </Square>
+      </div>
     </>
   )
 }
---
+++ src/components/Square.tsx
@@ -0,0 +1,16 @@
+type SquareProps = {
+    children: string;
+    handleClick?: () => void;
+};
+
+const Square = (props: SquareProps) => {
+  return (
+    <button 
+        className="square"
+        onClick={props.handleClick}
+    >
+        {props.children}
+    </button>
+  )
+}
+export default Square
