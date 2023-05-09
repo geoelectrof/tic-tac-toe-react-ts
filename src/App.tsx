e3c93e49453cf58a50748f9dff13096f8340cc71
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-05-09 16:31:24 +0300

    Finished handleclick functionality, ability to add X and O

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,25 @@ import Square from './components/Square'
 
 function App() {
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsPlaying, setXIsPlaying] = useState(true)
 
   function handleClick(e: React.MouseEvent<HTMLButtonElement>, index: number){
-    console.log("Clicked!" ,e , index)
+    // console.log("Clicked!" ,e.currentTarget , index)
+
+    //Prevents changing the value while clicking on an already clicked square
+    if (squares[index]) {
+      return
+    }
+    
+    const newSquares = squares.slice()
+    if (xIsPlaying) {
+      newSquares[index] = "X"
+    } else {
+      newSquares[index] = "O"   
+    }
+    setSquares(newSquares)
+    setXIsPlaying(!xIsPlaying)
   }
 
   return (
@@ -15,18 +30,18 @@ function App() {
       <div>App</div>  
       <div>       
         <Square index = {0} handleClick = { (e, index) => handleClick(e, index) }> { squares[0] } </Square>
-        {/* <Square>{squares[1]}</Square>
-        <Square>{squares[2]}</Square> */}
+        <Square index = {1} handleClick = { (e, index) => handleClick(e, index) }> { squares[1] } </Square>
+        <Square index = {2} handleClick = { (e, index) => handleClick(e, index) }> { squares[2] } </Square>
       </div>
       <div>       
-        {/* <Square>{squares[3]}</Square>
-        <Square>{squares[4]}</Square>
-        <Square>{squares[5]}</Square> */}
+        <Square index = {3} handleClick = { (e, index) => handleClick(e, index) }> { squares[3] } </Square>
+        <Square index = {4} handleClick = { (e, index) => handleClick(e, index) }> { squares[4] } </Square>
+        <Square index = {5} handleClick = { (e, index) => handleClick(e, index) }> { squares[5] } </Square>
       </div>
       <div>       
-        {/* <Square>{squares[6]}</Square>
-        <Square>{squares[7]}</Square>
-        <Square>{squares[8]}</Square> */}
+        <Square index = {6} handleClick = { (e, index) => handleClick(e, index) }> { squares[6] } </Square>
+        <Square index = {7} handleClick = { (e, index) => handleClick(e, index) }> { squares[7] } </Square>
+        <Square index = {8} handleClick = { (e, index) => handleClick(e, index) }> { squares[8] } </Square>
       </div>
     </>
   )
