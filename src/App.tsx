7b5212b47a93a8bedda61e8901b91615b05e643c
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-05-09 16:07:46 +0300

    Click functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
+import { useState } from 'react';
 import './App.css'
 import Square from './components/Square'
 
 function App() {
 
-  function handleClick(){
-    console.log("Clicked!")
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement>, index: number){
+    console.log("Clicked!" ,e , index)
   }
 
   return (
     <>
       <div>App</div>  
       <div>       
-        <Square handleClick={handleClick}> </Square>
-        <Square> </Square>
-        <Square> </Square>
+        <Square index = {0} handleClick = { (e, index) => handleClick(e, index) }> { squares[0] } </Square>
+        {/* <Square>{squares[1]}</Square>
+        <Square>{squares[2]}</Square> */}
       </div>
       <div>       
-        <Square> </Square>
-        <Square> </Square>
-        <Square> </Square>
+        {/* <Square>{squares[3]}</Square>
+        <Square>{squares[4]}</Square>
+        <Square>{squares[5]}</Square> */}
       </div>
       <div>       
-        <Square> </Square>
-        <Square> </Square>
-        <Square> </Square>
+        {/* <Square>{squares[6]}</Square>
+        <Square>{squares[7]}</Square>
+        <Square>{squares[8]}</Square> */}
       </div>
     </>
   )
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,17 +1,17 @@
 type SquareProps = {
-    children: string;
-    handleClick?: () => void;
+    index: number
+    children: React.ReactNode;
+    handleClick: (e: React.MouseEvent<HTMLButtonElement>, index: number) => void;
 };
 
 const Square = (props: SquareProps) => {
   return (
     <button 
         className="square"
-        onClick={props.handleClick}
+        onClick={(e) => props.handleClick(e, props.index)}
     >
         {props.children}
     </button>
-    
   )
 }
 export default Square
